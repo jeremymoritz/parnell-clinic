4fd4184358053230c556ff896ebfe2ba3b02d549
Jeremy Moritz <jeremy@jeremymoritz.com>
2015-03-27 11:16:02 -0500

    hello-parnell ready for show

--- src/js/helloparnell.jsx
+++ src/js/helloparnell.jsx
@@ -1,7 +1,8 @@
 React.render(
 	<div>
-		<h1>Hello, Parnell!</h1>
-		<img src="img/parnell-logo.jpg" alt />
+		<img src='img/parnell-logo.jpg' alt className='img-responsive' />
+		<h1>Hello Parnell!</h1>
+		<img src='img/parnell-footer.jpg' alt className='img-responsive' />
 	</div>,
 	document.getElementById('hello-parnell')
 );
